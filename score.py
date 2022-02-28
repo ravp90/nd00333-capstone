ef87178caebe206818747c0bf9594c2e36f32276
ravp90 <72314862+ravp90@users.noreply.github.com>
2022-02-28 10:46:31 +0000

    Add files via upload

---
+++ score.py
@@ -0,0 +1,22 @@
+import json
+import numpy as np
+import os
+import pickle
+import traceback
+import pandas as pd
+from sklearn.externals import joblib
+
+def init():
+    global model
+    model_path = os.path.join(os.getenv('AZUREML_MODEL_DIR'), 'automl_model.pkl')
+    model = joblib.load(model_path)
+    #with open(model_path, 'rb') as f:
+     #   model = pickle.load(f)
+
+def run(data):
+    try:
+        data = np.array(json.loads(data))
+        result = model.predict(data)
+        return result.tolist()
+    except Exception as e:
+        return str(e)
